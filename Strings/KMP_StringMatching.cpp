070066f494c466c220ca9048b54f7237ae82f746
shankar wangwad <shankarwangwad2018@gmail.com>
2023-04-07 20:35:31 +0530

    added kmp algo

---
+++ Strings/KMP_StringMatching.cpp
@@ -0,0 +1,61 @@
+//source --> youtube(tushar roy) link=https://www.youtube.com/watch?v=GTJr8OvyEVQ
+
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> calculateTempArray(string pattern)
+{
+       vector<int>lps=vector<int>((int)pattern.length(),0);
+
+       
+         int index =0;
+        for(int i=1; i < pattern.length();){
+            if(pattern[i] == pattern[index]){
+                lps[i] = index + 1;
+                index++;
+                i++;
+            }else{
+                if(index != 0){
+                    index = lps[index-1];
+                }else{
+                    lps[i] =0;
+                    i++;
+                }
+            }
+        }
+        return lps;    
+
+}
+
+
+bool KMP_StringMatching(string text, string pattern){
+        
+        vector<int>lps= calculateTempArray(pattern);
+        int i=0;
+        int j=0;
+        while(i < text.length() && j < pattern.length()){
+            if(text[i] == pattern[j]){
+                i++;
+                j++;
+            }else{
+                if(j!=0){
+                    j = lps[j-1];
+                }else{
+                    i++;
+                }
+            }
+        }
+        if(j == pattern.length()){
+            return true;
+        }
+        return false;
+    }
+int main()
+{
+
+       
+      string text="allshankarok";
+      string pattern="shankar";
+      cout<<KMP_StringMatching(text,pattern)<<endl;
+
+}
