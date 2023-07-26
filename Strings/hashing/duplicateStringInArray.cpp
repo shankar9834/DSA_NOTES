48d297bf95974ac7b1e3e7246c2f7a4d111d19b7
shankar wangwad <shankarwangwad2018@gmail.com>
2023-07-26 18:14:22 +0530

    added hashing

---
+++ Strings/hashing/duplicateStringInArray.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/*
+Problem: Given a list of n strings each no longer than m,find all the duplicate strings and divide them into groups.
+using hashing we can solve it in O(n*m+nlogn) 
+
+We calculate the hash for each string, sort the hashes together with the indices, and then group the indices by identical hashes
+*/
+
+
+long long compute_hash(string const& s) {
+    const int p = 31;
+    const int m = 1e9 + 9;
+    long long hash_value = 0;
+    long long p_pow = 1;
+    for (char c : s) {
+        hash_value = (hash_value + (c - 'a' + 1) * p_pow) % m;
+        p_pow = (p_pow * p) % m;
+    }
+    return hash_value;
+}
+
+vector<vector<int>> group_identical_strings(vector<string> const& s) {
+    int n = s.size();
+    vector<pair<long long, int>> hashes(n);
+    for (int i = 0; i < n; i++)
+        hashes[i] = {compute_hash(s[i]), i};
+
+    sort(hashes.begin(), hashes.end());
+
+    vector<vector<int>> groups;
+    for (int i = 0; i < n; i++) {
+        if (i == 0 || hashes[i].first != hashes[i-1].first)
+            groups.emplace_back();
+        groups.back().push_back(hashes[i].second);
+    }
+    return groups;
+}
+
+
+int main()
+{
+
+}
