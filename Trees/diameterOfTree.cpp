50c4bad61f9bbdfce9e3df34eff5ee017fe7df30
shankar wangwad <shankarwangwad2018@gmail.com>
2023-08-02 18:23:24 +0530

    some modification

--- Trees/diameterOfTree.cpp
+++ Trees/diameterOfTree.cpp
@@ -1,6 +1,8 @@
 
 //method 1
-/*
+/*  
+   O(n) time complexity
+   
   start bfs from any node(u) and find depth of all the other nodes ,after that choose the node with maximum depth(node v)
   and then start the bfs from node v and find depth of all the other nodes . the maximum depth from v is the diameter of the tree 
 
